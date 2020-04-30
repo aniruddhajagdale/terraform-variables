e15cf01ca6d9cb555feb5bbf213feffbc38ead0e
Jagdale <aniruddha.jagdale@dxc.com>
2020-04-30 12:11:51 +0530

    Updated this file

--- Instances.tf
+++ Instances.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 resource "aws_instance" "ec2" {
   ami                  = "${var.amis[var.region]}"
-  instance_type                 = "t2.micro"
+  instance_type        = "t2.micro"
   key_name             = "${var.keys[var.region]}"
   iam_instance_profile = "s3-admin-access"
 }
